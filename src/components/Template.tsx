b70b6709f9db0a56ea8c7ed9ca0fe8e4d84b8f50
Jonyelson Lopes <jonyelson20@gmail.com>
2024-03-17 12:04:57 -0300

    feat: created the form for image searches

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
 const Footer = () => {
   return (
-    <footer className="bg-indigo-950 text-white py-4 mt-8">
+    <footer className="bg-indigo-950 text-white py-4 mt-8 fixed bottom-0 w-full">
       <div className="container mx-auto text-center">
         Desenvolvido por Jonyelson Lopes
       </div>

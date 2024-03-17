715d31ac22f59918163d36cd86c2af7068103516
Jonyelson Lopes <jonyelson20@gmail.com>
2024-03-16 21:56:56 -0300

    feat: created the communication layer with the api.

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,14 @@
+import { Template, ImageCard} from '@/components'
+
+export default function GaleriaPage() {
+  return (
+    <Template>
+      <section className='grid grid-cols-3 gap-8'>
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </section>
+      
+    </Template>
+  )
+}
